cfd33526975ec01fc9f983ebbd9d5e9cba4fbe37
Filip Gontko <fgontko@netsuite.com>
2022-04-07 11:10:05 +0200

    Creating credentials class

--- backend/credetntials.py
+++ backend/credetntials.py
@@ -3,4 +3,36 @@
 class Credentials:
     """
     Class representing credentials (site, username, password).
+
+    Attributes:
+        site: Site
+        username: Username
+        password: Password
+
+    Methods:
+        get_credentials: Retrieves credentials in a list.
+        update_site: Update site.
+        update_username: Update username.
+        update_password: Update password.
+        remove_credentials: Remove credentials.
     """
+
+    def __init__(self, site=None, username=None, password=None):
+        self.site = site
+        self.username = username
+        self.password = password
+
+    def get_credentials(self):
+        return list((self.site, self.username, self.password))
+
+    def update_site(self, site):
+        pass
+
+    def update_username(self, username):
+        pass
+
+    def update_password(self, password):
+        pass
+
+    def remove_credentials(self, site, username, password):
+        pass
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 # This is a sample Python script.
 
-def main():
-    return True
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main()
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
