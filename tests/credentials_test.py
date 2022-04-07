7a24609920cd1e1a32c0cb68fe6a7cf3226810ff
Filip Gontko <fgontko@netsuite.com>
2022-04-07 15:04:39 +0200

    almost all tests for credentials

--- tests/credentials_test.py
+++ tests/credentials_test.py
@@ -17,10 +17,35 @@ class CredentialsTest(unittest.TestCase):
 
     def test_create_not_empty_credentials(self):
         credentials = Credentials("google.com", "f1", "cucak")
-        message = "Some of the attribute is not None."
-        self.assertEquals(credentials.site, "google.com")
-        self.assertEquals(credentials.username, "f1")
-        self.assertEquals(credentials.password, "cucak")
+        self.assertEqual(credentials.site, "google.com")
+        self.assertEqual(credentials.username, "f1")
+        self.assertEqual(credentials.password, "cucak")
+
+    def test_get_credentials(self):
+        credentials = Credentials("google.com", "f1", "cucak")
+        message = "Get credentials didn't return list of credentials."
+        self.assertEqual(credentials.get_credentials(), ["google.com", "f1", "cucak"], message)
+
+    def test_update_site(self):
+        credentials = Credentials("google.com", "f1", "cucak")
+        credentials.update_site("f1.com")
+        message = "Get credentials didn't return updated list of credentials."
+        self.assertEqual(credentials.get_credentials(), ["f1.com", "f1", "cucak"], message)
+
+    def test_update_username(self):
+        credentials = Credentials("google.com", "f1", "cucak")
+        credentials.update_username("redbull")
+        message = "Get credentials didn't return updated list of credentials."
+        self.assertEqual(credentials.get_credentials(), ["google.com", "redbull", "cucak"], message)
+
+    def test_update_password(self):
+        credentials = Credentials("google.com", "f1", "cucak")
+        credentials.update_password("verstappen")
+        message = "Get credentials didn't return updated list of credentials."
+        self.assertEqual(credentials.get_credentials(), ["google.com", "f1", "verstappen"], message)
+
+    def test_remove_credentials(self):
+        pass
 
 
 if __name__ == '__main__':
