047e751fbb7512abb58b4c2bd4f063d47b9e8e28
Filip Gontko <fgontko@netsuite.com>
2022-04-11 16:15:19 +0200

    Added try except statements and documentation

--- backend/password_manager.py
+++ backend/password_manager.py
@@ -1,30 +1,73 @@
 from backend.crypto import compare_master_password_hash, create_master_key
-from backend.databases.master_key_database import check_user_record_exists
+from backend.databases.master_key_database import check_user_record_exists, create_table, insert_master_information
 
 
 class PasswordManager:
     """
     Class representing password manager.
     """
-    def __init__(self, user_logged_in=False, email=None, master_key_db=None, credentials_db=None):
+    def __init__(self, user_logged_in=False, email=None):
+        """
+        Initialize password manager.
+        Args:
+            user_logged_in: True if user is logged in, False otherwise.
+            email: E-mail address of the user.
+        """
         self.user_logged_in = user_logged_in
         self.email = email
-        self.master_key_db = master_key_db
-        self.credentials_db = credentials_db
 
     def sign_up(self):
-        create_master_key()
-        self.user_logged_in = True
-        return True
+        """
+        Sign up to the password manager.
+        Returns:
+            True if successful, False otherwise.
+        """
+        try:
+            create_table()
+            master_key_hash = create_master_key()
+            self.email = input("E-mail: ")
+            insert_master_information(master_key_hash, self.email)
+            self.user_logged_in = True
+            return True
+        except Exception as e:
+            return False
 
     def check_user_exists(self):
+        """
+        Check if user is registered.
+        Returns:
+            True if successful, False otherwise.
+        """
         return check_user_record_exists(self.email)
 
     def login(self):
-        self.email = input("E-mail: ")
-        if self.check_user_exists():
-            compare_master_password_hash()
-        self.user_logged_in = True
+        """
+        Login to the password manager.
+        Returns:
+            True if successful, False otherwise.
+        """
+        try:
+            self.email = input("E-mail: ")
+            if self.check_user_exists():
+                compare_master_password_hash()
+            self.user_logged_in = True
+            return True
+        except Exception as e:
+            return False
+
+    def check_user_logged_in(self):
+        """
+        Checks if user is logged into the password manager.
+        Returns:
+            True if successful, False otherwise.
+        """
+        return self.user_logged_in
 
     def logout(self):
+        """
+        Logout from password manager.
+        Returns:
+            None
+        """
         self.user_logged_in = False
+        # TODO: Show login screen
