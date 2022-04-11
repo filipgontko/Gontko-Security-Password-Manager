12c6443a042ad3f16ca1d909f370c836d5dfd4ad
Filip Gontko <fgontko@netsuite.com>
2022-04-11 11:31:52 +0200

    prepared test cases for crypto test

--- tests/crypto_test.py
+++ tests/crypto_test.py
@@ -13,6 +13,23 @@ class CryptoTestCase(unittest.TestCase):
         generate_crypto_key_base()
         self.assertEqual(key_exists(), True)
 
+    def test_load_crypto_key_base_from_file(self):
+        pass
+
+    def test_get_crypto_key(self):
+        pass
+
+    def test_encrypt_message(self):
+        pass
+
+    def test_decrypt_message(self):
+        pass
+
+    def test_has_key(self):
+        pass
+
+    def test_generate_password(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
