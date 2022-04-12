1947d16dfd002233b5f44d8b380c55d51ee4d02d
Filip Gontko <fgontko@netsuite.com>
2022-04-12 11:32:48 +0200

    Preparing unittests

--- tests/password_manager_test.py
+++ tests/password_manager_test.py
@@ -8,6 +8,33 @@ class PasswordManagerTestCase(unittest.TestCase):
         password_manager = PasswordManager()
         self.assertIsInstance(password_manager, PasswordManager)
 
+    def test_sign_up(self):
+        pass
+
+    def test_check_user_exists(self):
+        pass
+
+    def test_login(self):
+        pass
+
+    def test_logout(self):
+        pass
+
+    def test_user_logged_in(self):
+        pass
+
+    def test_add_new_credentials(self):
+        pass
+
+    def test_edit_credentials(self):
+        pass
+
+    def test_remove_credentials(self):
+        pass
+
+    def test_prepare_credentials(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
