2406d6f2be5a2799dddf0e6dfdc00aa9df7e5bcb
Filip Gontko <fgontko@netsuite.com>
2022-04-12 11:57:17 +0200

    Prepared master key db tests

---
+++ tests/databases/master_key_db_tests.py
@@ -0,0 +1,25 @@
+import unittest
+
+
+class MasterKeyDBTestCase(unittest.TestCase):
+    def test_create_table(self):
+        self.assertEqual(True, False)
+
+    def test_insert_master_information(self):
+        pass
+
+    def test_edit_master_information(self):
+        pass
+
+    def test_get_master_key_hash(self):
+        pass
+
+    def test_check_user_record_exists(self):
+        pass
+
+    def test_clear_table(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
