8d503515207e63b7bea46b315e3479085b6d6f90
Filip Gontko <fgontko@netsuite.com>
2022-04-14 15:13:50 +0200

    First test of GUI with Kivy

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from kivy.lang import Builder
+from kivymd.app import MDApp
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class PasswordManagerApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "BlueGray"
+        return Builder.load_file('frontend/login.kv')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+PasswordManagerApp().run()
