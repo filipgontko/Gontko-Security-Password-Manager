b830be8a9a06328a48e0fdbe845f1b20ea735401
Filip Gontko <fgontko@netsuite.com>
2022-04-29 15:09:50 +0200

    Fail securely in logged_in.py

--- frontend/logged_in.py
+++ frontend/logged_in.py
@@ -1,5 +1,6 @@
 from kivy.uix.screenmanager import Screen
 
+from backend.my_logger import logger
 from backend.password_manager import generate_password
 
 
@@ -21,27 +22,36 @@ class LoggedIn(Screen):
         """
         Initialize text fields when entering the view.
         """
-        self.ids.search_field.text = "\r"
-        self.ids.search_field.text = ""
+        try:
+            self.ids.search_field.text = "\r"
+            self.ids.search_field.text = ""
+        except Exception as e:
+            logger.error("Exception occurred during on_enter(). {}".format(e))
 
     def on_leave(self):
         """
         Clear the text fields, password generator and set password field to show masked password when leaving the view.
         """
-        self.ids.search_field.text = ""
-        self.ids.website.text = ""
-        self.ids.username.text = ""
-        self.ids.passwd.text = ""
-        self.ids.generate_pwd.text = ""
-        self.ids.strength_slider.value = 12
-        self.ids.passwd.password = True
-        self.ids.eye_icon.icon = "eye-off"
+        try:
+            self.ids.search_field.text = ""
+            self.ids.website.text = ""
+            self.ids.username.text = ""
+            self.ids.passwd.text = ""
+            self.ids.generate_pwd.text = ""
+            self.ids.strength_slider.value = 12
+            self.ids.passwd.password = True
+            self.ids.eye_icon.icon = "eye-off"
+        except Exception as e:
+            logger.error("Exception occurred during on_leave(). {}".format(e))
 
     def logout(self):
         """
         Log out of the password manager.
         """
-        self.password_manager.logout()
+        try:
+            self.password_manager.logout()
+        except Exception as e:
+            logger.error("Exception occurred during logout(). {}".format(e))
 
     def view_credentials(self, credential_id, website, username):
         """
@@ -51,11 +61,14 @@ class LoggedIn(Screen):
             website: Website.
             username: Username.
         """
-        self.parent.current = "creds_view"
-        self.parent.transition.direction = "left"
-        self.password_manager.credential_id = credential_id
-        self.password_manager.credential_site = website
-        self.password_manager.credential_username = username
+        try:
+            self.parent.current = "creds_view"
+            self.parent.transition.direction = "left"
+            self.password_manager.credential_id = credential_id
+            self.password_manager.credential_site = website
+            self.password_manager.credential_username = username
+        except Exception as e:
+            logger.error("Exception occurred during view_credentials(). {}".format(e))
 
     def add_credentials(self, site, username, password):
         """
@@ -65,7 +78,10 @@ class LoggedIn(Screen):
             username: Username.
             password: Password.
         """
-        self.password_manager.add_new_credentials(site, username, password)
+        try:
+            self.password_manager.add_new_credentials(site, username, password)
+        except Exception as e:
+            logger.error("Exception occurred while add_credentials(). {}".format(e))
 
     def generate_password(self, length=12):
         """
@@ -76,7 +92,11 @@ class LoggedIn(Screen):
         Returns:
             Password string.
         """
-        return generate_password(length)
+        try:
+            return generate_password(length)
+        except Exception as e:
+            logger.error("Exception occurred during password generation. {}".format(e))
+            return ""
 
     def set_list_credentials(self, text="", search=True):
         """
@@ -104,12 +124,15 @@ class LoggedIn(Screen):
             website: Website.
             username: Username.
         """
-        self.ids.rv.data.append(
-            {
-                "viewclass": "CustomTwoLineCredsListItem",
-                "text": website,
-                "secondary_text": "username: {}".format(username),
-                "on_release": lambda: self.view_credentials(cred_id, website, username),
-                "callback": lambda x: x
-            }
-        )
+        try:
+            self.ids.rv.data.append(
+                {
+                    "viewclass": "CustomTwoLineCredsListItem",
+                    "text": website,
+                    "secondary_text": "username: {}".format(username),
+                    "on_release": lambda: self.view_credentials(cred_id, website, username),
+                    "callback": lambda x: x
+                }
+            )
+        except Exception as e:
+            logger.error("Exception occurred during add_credential_item(). {}".format(e))
