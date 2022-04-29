39c001719969e141b2852265d8fbd1ae6926669c
Filip Gontko <fgontko@netsuite.com>
2022-04-29 11:00:33 +0200

    Updated docs for credentials.py

--- backend/credentials.py
+++ backend/credentials.py
@@ -18,13 +18,28 @@ class Credentials:
     """
 
     def __init__(self, site=None, username=None, password=None):
+        """
+        Initializes credentials
+        Args:
+            site: website
+            username: username
+            password: password
+        """
         self.site = site
         self.username = username
         self.password = password
 
     def get_credentials(self):
+        """
+        Get credentials to be viewed.
+        Returns:
+            List of credentials.
+        """
         logger.info("Getting credentials.")
         return list((self.site, self.username, self.password))
 
     def __del__(self):
+        """
+        Destructs credentials object.
+        """
         logger.info("Credentials object deleted from memory.")
