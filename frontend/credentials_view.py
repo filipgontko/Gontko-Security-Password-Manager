940ca8c2d33958dc7e543f8fe11a8e689014ff60
Filip Gontko <fgontko@netsuite.com>
2022-04-29 15:04:08 +0200

    Fail securely in credentials_view.py

--- frontend/credentials_view.py
+++ frontend/credentials_view.py
@@ -3,6 +3,7 @@ from kivymd.uix.button import MDRoundFlatButton, MDFillRoundFlatButton
 from kivymd.uix.dialog import MDDialog
 
 from backend.crypto import check_if_pwned, check_password_strength
+from backend.my_logger import logger
 from backend.password_manager import generate_password
 
 
@@ -26,27 +27,33 @@ class CredentialsView(Screen):
         """
         Initialize text fields, strength meter and check if pawned, when entering the view.
         """
-        self.ids.website.text = self.get_site()
-        self.ids.username.text = self.get_username()
-        self.ids.passwd.text = self.get_password()
-        self.show_password_strength()
-        if check_if_pwned(self.ids.passwd.text):
-            self.dialog = MDDialog(text="OOOPS! Your password has been pwned! Change it now")
-            self.dialog.open()
-            self.dialog = None
+        try:
+            self.ids.website.text = self.get_site()
+            self.ids.username.text = self.get_username()
+            self.ids.passwd.text = self.get_password()
+            self.show_password_strength()
+            if check_if_pwned(self.ids.passwd.text):
+                self.dialog = MDDialog(text="OOOPS! Your password has been pwned! Change it now")
+                self.dialog.open()
+                self.dialog = None
+        except Exception as e:
+            logger.error("Exception occurred during on_enter(). {}".format(e))
 
     def on_leave(self):
         """
         Clear the text fields, strength meter, check if pawned and set password field to show masked password
         when leaving the view.
         """
-        self.ids.generate_pwd.text = ""
-        self.ids.strength_slider.value = 12
-        self.ids.strength_meter.value = 0
-        self.ids.strength_word.text = ""
-        self.ids.passwd.password = True
-        self.ids.eye_icon.icon = "eye-off"
-        self.ids.pwned.text = ""
+        try:
+            self.ids.generate_pwd.text = ""
+            self.ids.strength_slider.value = 12
+            self.ids.strength_meter.value = 0
+            self.ids.strength_word.text = ""
+            self.ids.passwd.password = True
+            self.ids.eye_icon.icon = "eye-off"
+            self.ids.pwned.text = ""
+        except Exception as e:
+            logger.error("Exception occurred during on_leave(). {}".format(e))
 
     def get_site(self):
         """
@@ -72,22 +79,25 @@ class CredentialsView(Screen):
         """
         try:
             return self.password_manager.get_password_from_db(self.password_manager.credential_id)
-        except Exception as e:
+        except Exception:
             return ""
 
     def show_password_strength(self):
         """
         Show password strength.
         """
-        strength_word = check_password_strength(self.ids.passwd.text)
+        try:
+            strength_word = check_password_strength(self.ids.passwd.text)
 
-        if check_if_pwned(self.ids.passwd.text):
-            self.ids.pwned.text = "Your password has been pwned!"
-            strength_word = "Weak"
-        else:
-            self.ids.pwned.text = "Your password has not been pwned!"
+            if check_if_pwned(self.ids.passwd.text):
+                self.ids.pwned.text = "Your password has been pwned!"
+                strength_word = "Weak"
+            else:
+                self.ids.pwned.text = "Your password has not been pwned!"
 
-        self.set_strength_values(strength_word)
+            self.set_strength_values(strength_word)
+        except Exception as e:
+            logger.error("Exception occurred in show_password_strength(). {}".format(e))
 
     def set_strength_values(self, strength_word):
         """
@@ -95,17 +105,20 @@ class CredentialsView(Screen):
         Args:
             strength_word: Verbal description of the password strength.
         """
-        if strength_word == "Weak":
-            self.prepare_strength_values(25, [1, 0, 0, 1], strength_word)
+        try:
+            if strength_word == "Weak":
+                self.prepare_strength_values(25, [1, 0, 0, 1], strength_word)
 
-        if strength_word == "Moderate":
-            self.prepare_strength_values(50, [1, 0.9, 0, 1], strength_word)
+            if strength_word == "Moderate":
+                self.prepare_strength_values(50, [1, 0.9, 0, 1], strength_word)
 
-        if strength_word == "Strong":
-            self.prepare_strength_values(70, [0.5, 0.9, 0, 1], strength_word)
+            if strength_word == "Strong":
+                self.prepare_strength_values(70, [0.5, 0.9, 0, 1], strength_word)
 
-        if strength_word == "Very Strong":
-            self.prepare_strength_values(100, [0, 1, 0, 1], strength_word)
+            if strength_word == "Very Strong":
+                self.prepare_strength_values(100, [0, 1, 0, 1], strength_word)
+        except Exception as e:
+            logger.error("Exception occurred while setting strength values. {}".format(e))
 
     def prepare_strength_values(self, value, color, word):
         """
@@ -116,9 +129,12 @@ class CredentialsView(Screen):
             word: Verbal description of the password strength.
 
         """
-        self.ids.strength_meter.value = value
-        self.ids.strength_meter.color = color
-        self.ids.strength_word.text = word
+        try:
+            self.ids.strength_meter.value = value
+            self.ids.strength_meter.color = color
+            self.ids.strength_word.text = word
+        except Exception as e:
+            logger.error("Exception occurred while preparing strength value, color and word. {}".format(e))
 
     def show_dialog(self, reason):
         """
@@ -126,10 +142,13 @@ class CredentialsView(Screen):
         Args:
             reason: Delete or Save.
         """
-        self.dialog = None
-        if not self.dialog:
-            self.set_dialog_context(reason)
-        self.dialog.open()
+        try:
+            self.dialog = None
+            if not self.dialog:
+                self.set_dialog_context(reason)
+            self.dialog.open()
+        except Exception as e:
+            logger.error("Exception occurred during show_dialog(). {}".format(e))
 
     def set_dialog_context(self, reason):
         """
@@ -137,24 +156,27 @@ class CredentialsView(Screen):
         Args:
             reason: Delete or Save.
         """
-        if reason == "delete":
-            self.dialog = MDDialog(
-                title="Delete credentials?",
-                text="Are you sure you want to delete these credentials forever?",
-                buttons=[
-                    MDRoundFlatButton(text="CANCEL", on_release=self.close_dialog),
-                    MDFillRoundFlatButton(text="DELETE", on_release=self.delete_credentials)
-                ],
-            )
-        if reason == "save":
-            self.dialog = MDDialog(
-                title="Edit credentials?",
-                text="Are you sure you want to overwrite current credentials?",
-                buttons=[
-                    MDRoundFlatButton(text="CANCEL", on_release=self.close_dialog),
-                    MDFillRoundFlatButton(text="SAVE", on_release=self.save_credentials)
-                ],
-            )
+        try:
+            if reason == "delete":
+                self.dialog = MDDialog(
+                    title="Delete credentials?",
+                    text="Are you sure you want to delete these credentials forever?",
+                    buttons=[
+                        MDRoundFlatButton(text="CANCEL", on_release=self.close_dialog),
+                        MDFillRoundFlatButton(text="DELETE", on_release=self.delete_credentials)
+                    ],
+                )
+            if reason == "save":
+                self.dialog = MDDialog(
+                    title="Edit credentials?",
+                    text="Are you sure you want to overwrite current credentials?",
+                    buttons=[
+                        MDRoundFlatButton(text="CANCEL", on_release=self.close_dialog),
+                        MDFillRoundFlatButton(text="SAVE", on_release=self.save_credentials)
+                    ],
+                )
+        except Exception as e:
+            logger.error("Exception occurred while setting dialog context. {}".format(e))
 
     def close_dialog(self, obj):
         """
@@ -162,8 +184,11 @@ class CredentialsView(Screen):
         Args:
             obj: Dialog object.
         """
-        self.dialog.dismiss()
-        self.dialog = None
+        try:
+            self.dialog.dismiss()
+            self.dialog = None
+        except Exception as e:
+            logger.error("Exception occurred while closing dialog. {}".format(e))
 
     def save_credentials(self, obj):
         """
@@ -171,12 +196,15 @@ class CredentialsView(Screen):
         Args:
             obj: Dialog object.
         """
-        self.dialog.dismiss()
-        self.dialog = None
-        self.password_manager.edit_credentials(self.password_manager.credential_id,
-                                               self.ids.website.text,
-                                               self.ids.username.text,
-                                               self.ids.passwd.text)
+        try:
+            self.dialog.dismiss()
+            self.dialog = None
+            self.password_manager.edit_credentials(self.password_manager.credential_id,
+                                                   self.ids.website.text,
+                                                   self.ids.username.text,
+                                                   self.ids.passwd.text)
+        except Exception as e:
+            logger.error("Exception occurred while saving credentials. {}".format(e))
 
     def delete_credentials(self, obj):
         """
@@ -184,11 +212,14 @@ class CredentialsView(Screen):
         Args:
             obj: Dialog object.
         """
-        self.dialog.dismiss()
-        self.dialog = None
-        self.password_manager.remove_credentials(self.password_manager.credential_id)
-        self.parent.current = "logged_in"
-        self.parent.transition.direction = "right"
+        try:
+            self.dialog.dismiss()
+            self.dialog = None
+            self.password_manager.remove_credentials(self.password_manager.credential_id)
+            self.parent.current = "logged_in"
+            self.parent.transition.direction = "right"
+        except Exception as e:
+            logger.error("Exception occurred while deleting credentials. {}".format(e))
 
     def generate_password(self, length=12):
         """
