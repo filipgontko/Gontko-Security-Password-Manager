8e20c3bb0e4ebfa796381c5c13cafa019a3a60e6
Filip Gontko <fgontko@netsuite.com>
2022-04-29 15:15:20 +0200

    Fail securely in main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from backend.my_logger import logger
 from backend.password_manager import PasswordManager
 from frontend.pm_app import PasswordManagerApp
 
@@ -6,8 +7,12 @@ def main():
     """
     The main function of the password manager.
     """
-    password_manager = PasswordManager()
-    PasswordManagerApp(password_manager).run()
+    try:
+        password_manager = PasswordManager()
+        PasswordManagerApp(password_manager).run()
+    except Exception as e:
+        logger.error("Exception occurred in main(). {}".format(e))
+        return None
 
 
 if __name__ == "__main__":
