364c4c63228144c0f0b1d8771d6fe8abb66c0714
Filip Gontko <fgontko@netsuite.com>
2022-04-29 11:04:46 +0200

    Saved todo for my_logger.py

--- backend/my_logger.py
+++ backend/my_logger.py
@@ -22,6 +22,7 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
 # Creating logger which will be used in other files.
 file_handler = logging.FileHandler(LOG_FILE)
+# TODO: Remove the streamHandler at the end for the file logging to work
 file_handler = logging.StreamHandler(sys.stdout)
 file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(formatter)
