5b50ca66f39ebfa9a2ddf2891b5f9c7f88f4d688
Filip Gontko <filip.gontko@gmail.com>
2022-05-18 12:13:39 +0200

    Password-less switch disables master password text box

--- frontend/signup.py
+++ frontend/signup.py
@@ -16,6 +16,7 @@ class Signup(Screen):
         """
         super(Signup, self).__init__()
         self.password_manager = password_manager
+        self.original_color = None
 
     def signup(self, username, password):
         """
@@ -25,9 +26,10 @@ class Signup(Screen):
             password: Master password.
         """
         try:
-            password = chacha20_encrypt(password)
-            if self.password_manager.sign_up(username, password):
-                self.setup_mfa()
+            if username != "" and password != "":
+                password = chacha20_encrypt(password)
+                if self.password_manager.sign_up(username, password):
+                    self.setup_mfa()
         except Exception as e:
             logger.error("Exception occurred during signup. {}".format(e))
 
@@ -42,3 +44,24 @@ class Signup(Screen):
             self.parent.transition.direction = "left"
         except Exception as e:
             logger.error("Exception occurred during setting up MFA. {}".format(e))
+
+    def password_less_switch(self, switch_object, switch_value, color):
+        """
+        Switch that enables password-less signup. If is active, master password input is disabled.
+        Args:
+            switch_object: Switch object
+            switch_value: Value from the switch (True if active, False if inactive)
+            color: Original color of the password text box
+        """
+        try:
+            if color != [0, 0, 0, 1]:
+                self.original_color = color
+
+            if switch_value:
+                self.ids.password.disabled = True
+                self.ids.password.normal_color = [0, 0, 0, 1]
+            else:
+                self.ids.password.disabled = False
+                self.ids.password.normal_color = self.original_color
+        except Exception as e:
+            logger.error("Exception occurred when switching password-less. - {}".format(e))
